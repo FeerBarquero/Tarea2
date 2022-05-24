87bfe3910cd885a49532cae6e21f26976643294a
Fernanda B <fernandab@grupomundial.cr>
2022-05-24 12:50:07 -0600

    first commit

---
+++ Tarea2.js
@@ -0,0 +1,46 @@
+// Array .concat()
+//Se usa para unir dos o más arrays.
+//No cambia los arrays existentes, sino que devuelve un nuevo array.
+
+// const num = [1, 2, 3];
+// const numIn = ['one', 'two','three'];
+// const numEs = ['uno', 'dos', 'tres'];
+// const numbers = num.concat(numIn, numEs);
+
+// console.log(numbers);
+
+//-----------------------------------------------------------------------
+
+//Array .Pop()
+//Elimina el último elemento de un array y lo devuelve.
+//Cambia la longitud del array.
+
+// var frutas = ['manzana', 'pera', 'sandia', 'mandarina'];
+// console.log(frutas); 
+
+// var ultimaFruta = frutas.pop();
+
+// console.log(ultimaFruta); 
+// console.log(frutas); 
+
+//-------------------------------------------------------------------------
+
+//Array .Push()
+//Añade uno o más elementos al final de un array
+//Devuelve la nueva longitud del array.
+
+// var alimentos = ['pollo', 'arroz'];
+// console.log(alimentos); 
+
+// var alimentosMas = alimentos.push('frijoles', 'carne');
+// console.log(alimentos); 
+// console.log(alimentosMas);  
+
+//--------------------------------------------------------------------------
+
+//Array .toString()
+//Devuelve una cadena de caracteres representando el array especificado y sus elementos.
+
+// const array = ['Hola','Juana',45];
+// console.log(array.toString());
+
